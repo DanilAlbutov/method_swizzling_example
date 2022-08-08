51e9bc5fc612333eb0391903adafee1d934a8cf5
Данил Албутов <>
2022-08-08 13:10:30 +0400

    initial commit

---
+++ MethodSwizzlingExample/CustomMethodSwizzler.swift
@@ -0,0 +1,17 @@
+//
+//  CustomMethodSwizzler.swift
+//  MethodSwizzlingExample
+//
+//  Created by Данил Албутов on 08.08.2022.
+//
+
+import Foundation
+
+class CustomMethodSwizzler {
+    
+    func swizzleMethods() {
+        
+    }
+    
+    private func original
+}
